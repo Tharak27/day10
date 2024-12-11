d5f2a9d7637e4313402be3a6d6a90cf6a00613aa
Tharak Ram <tharakarame@gmail.com>
2024-12-11 10:36:09 +0530

    Add files via upload

---
+++ nonvoid.java
@@ -0,0 +1,10 @@
+class Main{
+     public static String s()
+    {
+        return"hello";
+   }
+   public static void main(String args[]){
+    String res=s();
+    System.out.println(res);
+   }
+}
